7d059987960557e7b750335c8e8d32d976bccee1
RadimYassin <radimyassin20@gamil.com>
2023-01-14 22:41:26 +0100

    update in GlobleStyle

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, Input } from '../GlobalStyle'
 
 function AddTodo() {
+
+  const [value,setValue]=useState('')
   return (
     <Form>
       <Input type='text' pd bg='blue'  />
-      <Button pd primary>Add</Button>
+      <Button pd primary disabled={!value}>Add</Button>
     </Form>
   )
 }
