86a5cd2d842e4e30467eb75ef9b83a5f51be50b2
RadimYassin <radimyassin20@gamil.com>
2023-01-16 00:33:11 +0100

    add file redux in src folder

---
+++ src/redux/reducer/todos.js
@@ -0,0 +1,18 @@
+const initialState={
+    todos:[
+        {
+            id:1,
+            content:'salam',
+            completed:false
+        }
+    ]
+}
+
+export default todos = (state=initialState,action) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
